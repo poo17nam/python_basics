64a2e1f3b6362f41a41380385bc4cc9226e67f3d
Poonam Chauhan <poonam@localhost.localdomain>
2017-07-30 11:24:18 +0530

    function_params changes

--- function_params.py
+++ function_params.py
@@ -1,4 +1,4 @@
-def print_msg(msg, error="No error"):
+def print_msg(msg, error="No error", *kwargs):
     print("Log:" +error +"\t"+ msg)
 
-print_msg("This will be logged")
+print_msg("This will be logged","file not found","1","2")
